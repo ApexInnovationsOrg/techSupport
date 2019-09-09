438845a47e92f94dc99f8d5b29350abca13617aa
eddimull <eddimull@gmail.com>
2019-09-09 10:45:31 -0500

    jesus that's a terrible 'fix' for laravel 5.1

--- techSupportLaravel/app/Http/Controllers/ScheduledReports.php
+++ techSupportLaravel/app/Http/Controllers/ScheduledReports.php
@@ -4,7 +4,11 @@
  *
  * Created: JK April 2019
  */	
- 
+if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
+    // Ignores notices and reports all other kinds... and warnings
+    error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+    // error_reporting(E_ALL ^ E_WARNING); // Maybe this is enough
+}
 namespace App\Http\Controllers;
 
 use DB;
